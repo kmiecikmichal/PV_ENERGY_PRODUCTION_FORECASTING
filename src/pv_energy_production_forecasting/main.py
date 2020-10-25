11ebc1ec06372ad059d15a23272469776a7e1c9f
kmiecikmichal <kmiecikmichal.mk@gmail.com>
2020-10-25 23:15:31 +0100

    User class develompent v1

--- src/pv_energy_production_forecasting/main.py
+++ src/pv_energy_production_forecasting/main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from user import User
+
+
+print("Hello, database contains two users ")
+Michal = User("U001", "Michal", 20000, 20, 40, 40.002, 21.008)
+Piotr = User("U002", "Piotr", 10000, 20, 40, 40.002, 21.008)
+
+Michal.add_to_database()
+Piotr.add_to_database()
+Piotr.delete_from_database()
