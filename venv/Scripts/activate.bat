94630bfc276679daae17256c1ce8bb39a7241f30
Spectra <michal.kmiecik@geotronics.com.pl>
2020-10-27 16:47:38 +0100

    Test Commit Spectra

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Michal\PycharmProjects\PV_ENERGY_PRODUCTION_FORECASTING\venv
+set VIRTUAL_ENV=C:\Users\Spectra\PycharmProjects\PV_ENERGY_PRODUCTION_FORECASTING\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
